ee53d2407cb6c8f3142f4c250d5021b8c20fee2a
Octave Larose <octave.larose@hotmail.fr>
2021-12-14 15:35:16 +0000

    day 14 part 2, improved a bit further

--- 2021/day14.py
+++ 2021/day14.py
@@ -3,31 +3,29 @@ import math
 from collections import Counter
 
 
-def grow_polymer(template_str, pairs, nbr_steps):
-    template = dict(Counter([template_str[i:i + 2] for i in range(len(template_str) - 1)]))
+def grow_polymer(template_p, pairs, nbr_steps):
     for idx in range(nbr_steps):
-        new_template = {}
-        for name, val in template.items():
-            if name not in pairs:
-                continue
+        new_template_p = {}
+        for name, val in template_p.items():
             for v in [name[0] + pairs.get(name), pairs.get(name) + name[1]]:
-                new_template[v] = new_template[v] + template.get(name) if v in new_template else template.get(name)
+                new_template_p[v] = new_template_p.get(v, 0) + template_p.get(name)
+        template_p = new_template_p
 
-        template = new_template
+    char_count = {}
+    for pair_val, pair_nbr in template_p.items():
+        for c in pair_val:
+            char_count[c] = char_count.get(c, 0) + pair_nbr
 
-    fd = {}
-    for pair_val, pair_nbr in template.items():
-        fd[pair_val[0]] = pair_nbr if pair_val[0] not in fd else pair_nbr + fd[pair_val[0]]
-        fd[pair_val[1]] = pair_nbr if pair_val[1] not in fd else pair_nbr + fd[pair_val[1]]
-    return math.ceil(max(fd.values()) / 2) - math.ceil(min(fd.values()) / 2)
+    return math.ceil(max(char_count.values()) / 2) - math.ceil(min(char_count.values()) / 2)
 
 
 def main():
     template_str, pairs_str = open("inputs/input14", "r").read().split("\n\n")
+    template_pairs = dict(Counter([template_str[i:i + 2] for i in range(len(template_str) - 1)]))
     pairs = {p[0]: p[1] for p in map(lambda l: l.split(' -> '), pairs_str.split("\n"))}
 
-    print("Part 1:", grow_polymer(template_str, pairs, 10))
-    print("Part 2:", grow_polymer(template_str, pairs, 40))
+    print("Part 1:", grow_polymer(template_pairs, pairs, 10))
+    print("Part 2:", grow_polymer(template_pairs, pairs, 40))
 
 
 if __name__ == "__main__":
