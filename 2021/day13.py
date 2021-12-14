ae56eb1ee4e5124b45d955daab02dc983bcd1938
OctaveLarose <octave.larose@hotmail.fr>
2021-12-14 12:36:55 +0000

    day13 part 2

--- 2021/day13.py
+++ 2021/day13.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python
 
 
-def part1(coords, folds):
-    new_coords = []
+def fold(coords, folds, max_folds=None):
+    for idx, fold in enumerate(folds):
+        new_coords = []
+        for x, y in coords:
+            if fold[0] == 'x':
+                new_coords.append((x, y) if x < fold[1] else (2 * fold[1] - x, y))
+            else:
+                new_coords.append((x, y) if y < fold[1] else (x, 2 * fold[1] - y))
 
-    for fold in folds:
-        if fold[0] == 'x':
-            for x, y in coords:
-                if x < fold[1]:
-                    new_coords.append((x, y))
-                else:
-                    new_coords.append((2 * fold[1] - x, y))
-        else:
-            for x, y in coords:
-                if y < fold[1]:
-                    new_coords.append((x, y))
-                else:
-                    new_coords.append((x, 2 * fold[1] - y))
+        coords = set(new_coords)
 
-        return len(set(new_coords))
+        if max_folds is not None and idx >= max_folds - 1:
+            return len(coords)
+
+    return coords
+
+
+def print_coords(coords):
+    map_x, map_y = (max(l) + 1 for l in zip(*coords))
+    map = [["."] * map_x for _ in range(map_y)]
+    for x, y in coords:
+        map[y][x] = '#'
+    print(*map, sep='\n')
 
 
 def main():
@@ -26,7 +31,8 @@ def main():
     coords = [(int(c[0]), int(c[1])) for c in map(lambda l: l.split(','), coords_str.split("\n"))]
     folds = [(f[0], int(f[1])) for f in map(lambda l: l.replace("fold along ", "").split("="), folds_str.split('\n'))]
 
-    print("Part 1:", part1(coords, folds))
+    print("Part 1:", fold(coords, folds, 1))
+    print("Part 2:"), print_coords(fold(coords, folds))
 
 
 if __name__ == "__main__":
