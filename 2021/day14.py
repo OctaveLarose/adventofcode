ebe980b00c1224246a08ab10730370d3a91be87a
Octave Larose <octave.larose@hotmail.fr>
2021-12-14 15:27:19 +0000

    day 13 part 2, optimized solution

--- 2021/day14.py
+++ 2021/day14.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python
+import math
 from collections import Counter
 
-NBR_STEPS = 10
 
+def grow_polymer(template_str, pairs, nbr_steps):
+    template = dict(Counter([template_str[i:i + 2] for i in range(len(template_str) - 1)]))
+    for idx in range(nbr_steps):
+        new_template = {}
+        for name, val in template.items():
+            if name not in pairs:
+                continue
+            for v in [name[0] + pairs.get(name), pairs.get(name) + name[1]]:
+                new_template[v] = new_template[v] + template.get(name) if v in new_template else template.get(name)
 
-def grow_polymer(template, pairs):
-    for idx in range(NBR_STEPS):
-        new_template = ""
-        for subt in [template[i:i + 2] for i in range(len(template) - 1)]:
-            new_template += subt[0]
-            if ''.join(subt) in pairs:
-                new_template += pairs.get(''.join(subt))
-        new_template += template[-1]
         template = new_template
 
-    c = Counter(template)
-    return max(c.values()) - min(c.values())
+    fd = {}
+    for pair_val, pair_nbr in template.items():
+        fd[pair_val[0]] = pair_nbr if pair_val[0] not in fd else pair_nbr + fd[pair_val[0]]
+        fd[pair_val[1]] = pair_nbr if pair_val[1] not in fd else pair_nbr + fd[pair_val[1]]
+    return math.ceil(max(fd.values()) / 2) - math.ceil(min(fd.values()) / 2)
 
 
 def main():
-    template, pairs_str = open("inputs/input14", "r").read().split("\n\n")
+    template_str, pairs_str = open("inputs/input14", "r").read().split("\n\n")
     pairs = {p[0]: p[1] for p in map(lambda l: l.split(' -> '), pairs_str.split("\n"))}
 
-    print("Part 1:", grow_polymer(template, pairs))
+    print("Part 1:", grow_polymer(template_str, pairs, 10))
+    print("Part 2:", grow_polymer(template_str, pairs, 40))
 
 
 if __name__ == "__main__":
