d71a20e9177131801df9078594e390b14ea369dd
OctaveLarose <octave.larose@hotmail.fr>
2021-12-14 13:49:46 +0000

    day 14 part 1, fairly easy

--- 2021/day14.py
+++ 2021/day14.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python
+from collections import Counter
 
-NBR_STEPS = 4
+NBR_STEPS = 10
 
 
 def grow_polymer(template, pairs):
-    for i in range(NBR_STEPS):
+    for idx in range(NBR_STEPS):
         new_template = ""
-        new_template = template
-    return template
+        for subt in [template[i:i + 2] for i in range(len(template) - 1)]:
+            new_template += subt[0]
+            if ''.join(subt) in pairs:
+                new_template += pairs.get(''.join(subt))
+        new_template += template[-1]
+        template = new_template
+
+    c = Counter(template)
+    return max(c.values()) - min(c.values())
 
 
 def main():
-    template, pairs_str = open("inputs/tests/testinput14", "r").read().split("\n\n")
-    pairs = [(p[0], p[1]) for p in map(lambda l: l.split(' -> '), pairs_str.split("\n"))]
+    template, pairs_str = open("inputs/input14", "r").read().split("\n\n")
+    pairs = {p[0]: p[1] for p in map(lambda l: l.split(' -> '), pairs_str.split("\n"))}
 
     print("Part 1:", grow_polymer(template, pairs))
 
