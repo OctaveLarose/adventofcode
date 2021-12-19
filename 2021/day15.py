f62be29140f0570ca71d36077d710e4603ec05fb
Octave Larose <octave.larose@hotmail.fr>
2021-12-19 21:18:27 +0000

    day 15 part 2 skeleton and improvements to part 1

--- 2021/day15.py
+++ 2021/day15.py
@@ -1,52 +1,49 @@
 #!/usr/bin/python
 import sys
+from math import sqrt
 from queue import PriorityQueue
 
-def get_adjacent(coord, cavern):
-    x, y = coord
-    coords = [(x, y - 1), (x - 1, y), (x, y + 1), (x + 1, y)]
-    return list(filter(lambda t: 0 <= t[0] < len(cavern[0]) and 0 <= t[1] < len(cavern[0]), [(x1, y1) for x1, y1 in coords]))
 
-
-def get_min_cost_vertex(vertices):
-    return vertices.index(min(vertices))
+def get_adjacent(coord, cavern_size):
+    x, y = coord % cavern_size, coord // cavern_size
+    coords = filter(lambda t: all([0 <= v < cavern_size for v in t]), [(x, y - 1), (x - 1, y), (x, y + 1), (x + 1, y)])
+    return [y * cavern_size + x for (x, y) in coords]
 
 
 def dijkstra(cavern):
-    SIZE = len(cavern)
-    vertices = {i: sys.maxsize for i in range(SIZE**2)}
-    vertices[0] = 0
+    cavern_size = int(sqrt(len(cavern)))
+    vertices = {i: 0 if i == 0 else sys.maxsize for i in range(cavern_size**2)}
     visited_vertices = []
 
     pq = PriorityQueue()
-    pq.put((0, (0, 0)))
+    pq.put((0, 0))
 
     while not pq.empty():
         (dist, current_vertex) = pq.get()
         visited_vertices.append(current_vertex)
-        x1, y1 = current_vertex
-        current_vertex_id = SIZE * y1 + x1
 
-        adjs = get_adjacent(current_vertex, cavern)
+        adjs = get_adjacent(current_vertex, cavern_size)
         for neighbor in adjs:
-            x, y = neighbor
-            neighbor_id = SIZE * y + x
-            distance = cavern[y][x]
+            distance = cavern[neighbor]
             if neighbor not in visited_vertices:
-                old_cost = vertices[neighbor_id]
-                new_cost = vertices[current_vertex_id] + distance
+                old_cost = vertices[neighbor]
+                new_cost = vertices[current_vertex] + distance
                 if new_cost < old_cost:
                     pq.put((new_cost, neighbor))
-                    vertices[neighbor_id] = new_cost
+                    vertices[neighbor] = new_cost
 
-    print(vertices)
-    return vertices[SIZE**2 - 1]
+    return vertices[cavern_size**2 - 1]
+
+
+def get_bigger_cavern(cavern):
+    return cavern  # TODO
 
 
 def main():
-    cavern = [[int(x) for x in l.strip()] for l in open("inputs/tests/testinput15", "r").readlines()]
+    cavern = [int(x) for x in list(open("inputs/tests/testinput15", "r").read().replace('\n', ''))]
 
     print("Part 1:", dijkstra(cavern))
+    print("Part 2:", dijkstra(get_bigger_cavern(cavern)))
 
 
 if __name__ == "__main__":
