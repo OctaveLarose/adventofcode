bccf2708aba3e390c358bee06a2b05e5d704fb8c
Octave Larose <octave.larose@hotmail.fr>
2021-12-20 12:10:11 +0100

    day 15 part 2: a solution that works but is slow and ugly. needs a* instead of dijkstra, most likely...

--- 2021/day15.py
+++ 2021/day15.py
@@ -10,7 +10,7 @@ def get_adjacent(coord, cavern_size):
     return [y * cavern_size + x for (x, y) in coords]
 
 
-def dijkstra(cavern):
+def slow_and_ugly_dijkstra(cavern):
     cavern_size = int(sqrt(len(cavern)))
     vertices = {i: 0 if i == 0 else sys.maxsize for i in range(cavern_size**2)}
     visited_vertices = []
@@ -24,8 +24,8 @@ def dijkstra(cavern):
 
         adjs = get_adjacent(current_vertex, cavern_size)
         for neighbor in adjs:
-            distance = cavern[neighbor]
             if neighbor not in visited_vertices:
+                distance = cavern[neighbor]
                 old_cost = vertices[neighbor]
                 new_cost = vertices[current_vertex] + distance
                 if new_cost < old_cost:
@@ -36,14 +36,26 @@ def dijkstra(cavern):
 
 
 def get_bigger_cavern(cavern):
-    return cavern  # TODO
+    cavern_size = int(sqrt(len(cavern)))
+    bigger_cavern = []
+
+    for i in range(cavern_size):
+        chunk = cavern[i * cavern_size:i * cavern_size + cavern_size]
+        for j in range(5):
+            bigger_cavern += map(lambda x: x + j if x + j <= 9 else (x + j) - 9, chunk)
+
+    new_bigger_cavern = []
+    for i in range(5):
+        new_bigger_cavern += map(lambda x: x + i if x + i <= 9 else (x + i) - 9, bigger_cavern)
+
+    return new_bigger_cavern
 
 
 def main():
-    cavern = [int(x) for x in list(open("inputs/tests/testinput15", "r").read().replace('\n', ''))]
+    cavern = [int(x) for x in list(open("inputs/input15", "r").read().replace('\n', ''))]
 
-    print("Part 1:", dijkstra(cavern))
-    print("Part 2:", dijkstra(get_bigger_cavern(cavern)))
+    print("Part 1:", slow_and_ugly_dijkstra(cavern))
+    print("Part 2:", slow_and_ugly_dijkstra(get_bigger_cavern(cavern)))
 
 
 if __name__ == "__main__":
