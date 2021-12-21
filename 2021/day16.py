5dd48441ab78c4f54ee0e93c112a32e4da77dbf8
Octave Larose <octave.larose@hotmail.fr>
2021-12-21 12:29:35 +0100

    day 16: literal parsing and half of operator parsing

--- 2021/day16.py
+++ 2021/day16.py
@@ -3,56 +3,86 @@
 class Packet:
     LITERAL_ID = 4
 
-    packet_str = ''
+    packet_str = None
     bin_str = ''
 
     packet_version = None
     packet_type_id = None
-    subpackets = []
 
-    def __init__(self, packet_str):
-        self.packet_str = packet_str
-        self.bin_str = bin(int(self.packet_str, 16))[2:]
+    # Those two warrant creating subclasses
+    subpackets = []
+    packet_value = None
+
+    packet_len = None
+
+    def __init__(self, input_str):
+        if all([c in '01' for c in input_str]):
+            self.bin_str = input_str
+        else:
+            self.packet_str = input_str
+            self.bin_str = bin(int(self.packet_str, 16))[2:]
 
     def parse(self):
         self.packet_version = int(self.bin_str[:3], 2)
         self.packet_type_id = int(self.bin_str[3:6], 2)
 
-        if self.packet_version == self.LITERAL_ID:
+        self.packet_len = 6
+
+        if self.packet_type_id == self.LITERAL_ID:
             self.parse_literal()
         else:
             self.parse_operator()
         return self
 
     def parse_literal(self):
-        pass  # Not needed for part 1.
+        value_str = self.bin_str[6:]
+        nbr_bits = ''
+        for i in range(0, len(value_str), 5):
+            chunk = value_str[i:i + 5]
+            nbr_bits += chunk[1:]
+            self.packet_len += len(chunk)
+            if chunk[0] == '0':
+                break
+
+        self.packet_value = int(nbr_bits, 2)
 
     def parse_operator(self):
-        self.subpackets = self.get_subpackets()
+        self.process_subpackets()
         pass
 
-    def get_subpackets(self):
+    def process_subpackets(self):
         length_type_id = int(self.bin_str[7], 2)
         print(length_type_id)
         if length_type_id == 0:
             subpackets_nbr_bits = int(self.bin_str[7:20], 2)
-            print(subpackets_nbr_bits)
+            self.packet_len += 15 + subpackets_nbr_bits
+            subpackets_str = self.bin_str[20:20+subpackets_nbr_bits]
+            idx = 0
+            while idx <= len(subpackets_str) and not all([c == '0' for c in subpackets_str[idx:]]):
+                p = Packet(subpackets_str[idx:]).parse()
+                self.subpackets.append(p)
+                idx += p.get_packet_len()
         else:
             print("TODO implement")
-
-        return []
 
     def print_info(self):
         print(f"Packet version: {self.packet_version}")
         print(f"Packet type ID: {self.packet_type_id}")
+        print(f"Packet length: {self.packet_len}")
+        if self.packet_value is not None:
+            print(f"Packet value: {self.packet_value}")
+
+    def get_packet_len(self):
+        return self.packet_len
 
     def get_version_sum(self):
-        return self.packet_version + sum([sp.packet_version for sp in self.subpackets])
+        return self.packet_version + sum([sp.get_version_sum() for sp in self.subpackets])
 
 
 def main():
     packet_str = "38006F45291200"
     # packet_str = open("inputs/input16").read()
+
 
     # assert Packet("D2FE28").parse().get_version_sum() == 6
     # assert Packet("8A004A801A8002F478").parse().get_version_sum() == 16
