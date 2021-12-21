2fec8aaff136206aec2f4e57d489b13cbd5095c4
Octave Larose <octave.larose@hotmail.fr>
2021-12-21 15:46:09 +0100

    day 16 part 1 complete

--- 2021/day16.py
+++ 2021/day16.py
@@ -3,24 +3,21 @@
 class Packet:
     LITERAL_ID = 4
 
-    packet_str = None
-    bin_str = ''
-
-    packet_version = None
-    packet_type_id = None
-
-    # Those two warrant creating subclasses
-    subpackets = []
-    packet_value = None
-
-    packet_len = None
-
     def __init__(self, input_str):
         if all([c in '01' for c in input_str]):
             self.bin_str = input_str
         else:
             self.packet_str = input_str
-            self.bin_str = bin(int(self.packet_str, 16))[2:]
+            self.bin_str = ''.join([bin(int(c, 16))[2:].zfill(4) for c in self.packet_str])
+
+        # Those two warrant creating subclasses
+        self.subpackets = []
+        self.packet_value = None
+
+        self.packet_version = None
+        self.packet_type_id = None
+
+        self.packet_len = None
 
     def parse(self):
         self.packet_version = int(self.bin_str[:3], 2)
@@ -51,19 +48,27 @@ class Packet:
         pass
 
     def process_subpackets(self):
-        length_type_id = int(self.bin_str[7], 2)
-        print(length_type_id)
+        length_type_id = int(self.bin_str[6], 2)
+        self.packet_len += 1
         if length_type_id == 0:
-            subpackets_nbr_bits = int(self.bin_str[7:20], 2)
+            subpackets_nbr_bits = int(self.bin_str[7:22], 2)
             self.packet_len += 15 + subpackets_nbr_bits
-            subpackets_str = self.bin_str[20:20+subpackets_nbr_bits]
+            subpackets_str = self.bin_str[22:22 + subpackets_nbr_bits]
             idx = 0
             while idx <= len(subpackets_str) and not all([c == '0' for c in subpackets_str[idx:]]):
                 p = Packet(subpackets_str[idx:]).parse()
                 self.subpackets.append(p)
                 idx += p.get_packet_len()
         else:
-            print("TODO implement")
+            subpackets_nbr = int(self.bin_str[7:18], 2)
+            self.packet_len += 11
+            subpackets_str = self.bin_str[18:]
+            idx = 0
+            for i in range(subpackets_nbr):
+                p = Packet(subpackets_str[idx:]).parse()
+                self.subpackets.append(p)
+                idx += p.get_packet_len()
+                self.packet_len += p.get_packet_len()
 
     def print_info(self):
         print(f"Packet version: {self.packet_version}")
@@ -80,15 +85,13 @@ class Packet:
 
 
 def main():
-    packet_str = "38006F45291200"
-    # packet_str = open("inputs/input16").read()
+    packet_str = open("inputs/input16").read()
 
-
-    # assert Packet("D2FE28").parse().get_version_sum() == 6
-    # assert Packet("8A004A801A8002F478").parse().get_version_sum() == 16
-    # assert Packet("620080001611562C8802118E34").parse().get_version_sum() == 12
-    # assert Packet("C0015000016115A2E0802F182340").parse().get_version_sum() == 23
-    # assert Packet("A0016C880162017C3686B18A3D4780").parse().get_version_sum() == 31
+    assert Packet("D2FE28").parse().get_version_sum() == 6
+    assert Packet("8A004A801A8002F478").parse().get_version_sum() == 16
+    assert Packet("620080001611562C8802118E34").parse().get_version_sum() == 12
+    assert Packet("C0015000016115A2E0802F182340").parse().get_version_sum() == 23
+    assert Packet("A0016C880162017C3686B18A3D4780").parse().get_version_sum() == 31
 
     print("Part 1:", Packet(packet_str).parse().get_version_sum())
 
