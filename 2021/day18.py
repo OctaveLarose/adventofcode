302e5181cab722f2c3623628ffa27d50acd5b189
Octave Larose <octave.larose@hotmail.fr>
2021-12-22 18:12:53 +0100

    day 18: part 1 should work, but doesn't

--- 2021/day18.py
+++ 2021/day18.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 import ast
+import math
 
 SHOULD_EXPLODE = 1
 SHOULD_SPLIT = 2
-NOTHING_NEEDED = 2
+NOTHING_NEEDED = 3
 
 
 def get_magnitude(pair):
@@ -15,9 +16,6 @@ def get_magnitude(pair):
 def analyse_list(l, coords):
     if isinstance(l, int):
         return None, NOTHING_NEEDED
-
-    # if isinstance(l[0], int) or isinstance(l[1], int):
-    #     print(*coords, ',', len(coords), ':', l)
 
     if len(coords) >= 4:
         return coords, SHOULD_EXPLODE
@@ -52,77 +50,74 @@ def handle_explode(pair, coord):
     idx_right = coord.copy() if not all([c == 1 for c in coord]) else None
 
     if idx_left is not None:
-        idx_left = coord[:len(idx_left) - idx_left[::-1].index(1) - 1] + [1]
+        idx_left = idx_left[:len(idx_left) - idx_left[::-1].index(1) - 1] + [0]
         while isinstance(get_sublist_elem(pair, idx_left), list):
             idx_left += [1]
+
     if idx_right is not None:
-        idx_right = coord[:len(idx_right) - idx_right[::-1].index(0) - 1] + [1]
+        idx_right = idx_right[:len(idx_right) - idx_right[::-1].index(0) - 1] + [1]
         while isinstance(get_sublist_elem(pair, idx_right), list):
             idx_right += [0]
-        # idx_right[len(idx_right) - idx_right[::-1].index(0) - 1] = 1
-
-    # print('Left:', idx_left, ':', get_sublist_elem(pair, idx_left) if idx_left is not None else 0)
-    # print('Right:', idx_right, ':', get_sublist_elem(pair, idx_right) if idx_right is not None else 'N/A')
 
     x, y = get_sublist_elem(pair, coord)
-    parent_pair = get_sublist_elem(pair, coord[:-1])
-    # print(parent_pair)
-    if coord[-1] == 0:
-        replace_sublist_elem(pair, coord[:-1], [0, y + parent_pair[1]])
-        if idx_left is not None:
-            replace_sublist_elem(pair, idx_left, x + get_sublist_elem(pair, idx_left))
-    else:
-        replace_sublist_elem(pair, coord[:-1], [x + parent_pair[0], 0])
-        if idx_right is not None:
-            replace_sublist_elem(pair, idx_right, y + get_sublist_elem(pair, idx_right))
+
+    if idx_left is not None:
+        replace_sublist_elem(pair, idx_left, x + get_sublist_elem(pair, idx_left))
+    if idx_right is not None:
+        replace_sublist_elem(pair, idx_right, y + get_sublist_elem(pair, idx_right))
+
+    replace_sublist_elem(pair, coord, 0)
 
     return pair
 
 
 def handle_split(pair, coord):
     target_nbr = get_sublist_elem(pair, coord)
-    new_val = [target_nbr // 2, target_nbr // 2 if target_nbr % 2 == 0 else target_nbr // 2 + 1]
+    new_val = [target_nbr // 2, math.ceil(target_nbr / 2)]
     replace_sublist_elem(pair, coord, new_val)
     return pair
 
 
 def add(p1, p2):
     p = [p1] + [p2]
-    c, res = analyse_list(p, [])
+    res = None
     while res != NOTHING_NEEDED:
-        print(p)
+        c, res = analyse_list(p, [])
         if res == SHOULD_EXPLODE:
             p = handle_explode(p, c)
         elif res == SHOULD_SPLIT:
             p = handle_split(p, c)
-        c, res = analyse_list(p, [])
     return p
 
 
-def tests():
-    assert handle_explode([[[[[9, 8], 1], 2], 3], 4], [0, 0, 0, 0]) == [[[[0, 9], 2], 3], 4]
-    assert handle_explode([7, [6, [5, [4, [3, 2]]]]], [1, 1, 1, 1]) == [7, [6, [5, [7, 0]]]]
-    assert handle_explode([[3, [2, [1, [7, 3]]]], [6, [5, [4, [3, 2]]]]], [0, 1, 1, 1]) == [[3, [2, [8, 0]]], [9, [5, [4, [3, 2]]]]]
-    assert handle_explode([[3, [2, [8, 0]]], [9, [5, [4, [3, 2]]]]], [1, 1, 1, 1]) == [[3, [2, [8, 0]]], [9, [5, [7, 0]]]]
-
-    assert handle_split([[[[0, 7], 4], [15, [0, 13]]], [1, 1]], [0, 1, 0]) == [[[[0, 7], 4], [[7, 8], [0, 13]]], [1, 1]]
-    assert handle_split([[[[0, 7], 4], [[7, 8], [0, 13]]], [1, 1]], [0, 1, 1, 1]) == [[[[0, 7], 4], [[7, 8], [0, [6, 7]]]], [1, 1]]
+def run_tests():
+    # assert handle_explode([[[[[9, 8], 1], 2], 3], 4], [0, 0, 0, 0]) == [[[[0, 9], 2], 3], 4]
+    # assert handle_explode([7, [6, [5, [4, [3, 2]]]]], [1, 1, 1, 1]) == [7, [6, [5, [7, 0]]]]
+    # assert handle_explode([[3, [2, [1, [7, 3]]]], [6, [5, [4, [3, 2]]]]], [0, 1, 1, 1]) == [[3, [2, [8, 0]]], [9, [5, [4, [3, 2]]]]]
+    # assert handle_explode([[3, [2, [8, 0]]], [9, [5, [4, [3, 2]]]]], [1, 1, 1, 1]) == [[3, [2, [8, 0]]], [9, [5, [7, 0]]]]
+    # assert handle_explode([[[[0, 7], 4], [7, [[8, 4], 9]]], [1, 1]], [0, 1, 1, 0]) == [[[[0, 7], 4], [15, [0, 13]]], [1, 1]]
+    #
+    # assert handle_split([[[[0, 7], 4], [15, [0, 13]]], [1, 1]], [0, 1, 0]) == [[[[0, 7], 4], [[7, 8], [0, 13]]], [1, 1]]
+    # assert handle_split([[[[0, 7], 4], [[7, 8], [0, 13]]], [1, 1]], [0, 1, 1, 1]) == [[[[0, 7], 4], [[7, 8], [0, [6, 7]]]], [1, 1]]
+    #
+    # assert add([[[[4, 3], 4], 4], [7, [[8, 4], 9]]], [1, 1]) == [[[[0, 7], 4], [[7, 8], [6, 0]]], [8, 1]]
+    assert add([[[0, [4, 5]], [0, 0]], [[[4, 5], [2, 6]], [9, 5]]], [7, [[[3, 7], [4, 3]], [[6, 3], [8, 8]]]]) == [
+        [[[4, 0], [5, 4]], [[7, 7], [6, 0]]], [[8, [7, 7]], [[7, 9], [5, 0]]]]
 
 
 def main():
     # pairs = [ast.literal_eval(expr) for expr in open("inputs/input18").read().splitlines()]
-    # print(*pairs, sep='\n')
+    # pairs = [ast.literal_eval(expr) for expr in open("inputs/tests/testinput18").read().splitlines()]
 
-    # pair = [[[[8, 7], [7, 7]], [[8, 6], [7, 7]]], [[[0, 7], [6, 6]], [8, 7]]]
-    # print(get_magnitude(pair))
+    run_tests()
 
-    tests()
-
-    # pair = [[[[[9, 8], 1], 2], 3], 4]
-    # pair = [[3, [2, [1, [7, 3]]]], [6, [5, [4, [3, 2]]]]]
-    # pair = [[[[0, 7], 4], [15, [0, 13]]], [1, 1]]
-    # print(analyse_list(pair, []))
-    # print(add([[[[4, 3], 4], 4], [7, [[8, 4], 9]]], [1, 1]))
+    # s = pairs[0]
+    # for p in pairs[1:]:
+    #     print(s, '+++', p)
+    #     s = add(s, p)
+    #     print('=', s)
+    #
+    # print("Part 1:", get_magnitude(s))
 
 
 if __name__ == "__main__":
