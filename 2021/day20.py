338fbdccd0fc8d393b4668ee2e5155734df41db1
Octave Larose <octave.larose@hotmail.fr>
2021-12-25 21:20:57 +0100

    day 20 part 1, incorrect result on the actual input

--- 2021/day20.py
+++ 2021/day20.py
@@ -3,8 +3,30 @@ import itertools
 from functools import reduce
 
 
+def get_surrounding_coords(x, y):
+    return [(x - 1, y - 1), (x, y - 1), (x + 1, y - 1), (x - 1, y), (x, y), (x + 1, y), (x - 1, y + 1), (x, y + 1), (x + 1, y + 1)]
+    # coords = []
+    # for y1 in range(y - 1, y + 2):
+    #     for x1 in range(x - 1, x + 2):
+    #         coords.append((x1, y1))
+    # return coords
+    # return list(itertools.product(range(y - 1, y + 2), range(x - 1, x + 2)))
+
+
+def check_pixel(coord, inp, algo):
+    surr = get_surrounding_coords(*coord)
+    idx = sum([2**(8 - i) if surr[i] in inp else 0 for i in range(0, 9)])
+    return algo[idx] == '#'
+
+
 def enhance(inp, algo):
-    return inp
+    new_inp = set()
+    for p in inp:
+        surr = get_surrounding_coords(*p)
+        for s in surr:
+            if check_pixel(s, inp, algo):
+                new_inp.add(s)
+    return new_inp
 
 
 def enhance_n_times(inp, algo, n):
@@ -15,7 +37,14 @@ def enhance_n_times(inp, algo, n):
 
 def map_to_coords(inp):
     center = (len(inp[0]) // 2, len(inp) // 2)
-    return [(x - center[0], y - center[1]) for x, y in itertools.product(range(len(inp)), range(len(inp))) if inp[y][x] == 1]
+
+    coords = []
+    for y, l in enumerate(inp):
+        for x, c in enumerate(l):
+            if c == 1:
+                coords.append((x - center[0], y - center[1]))
+    return coords
+    # return [(x - center[0], y - center[1]) for x, y in itertools.product(range(len(inp)), range(len(inp))) if inp[y][x] == 1]
 
 
 def draw_map(coords):
@@ -29,15 +58,14 @@ def draw_map(coords):
 
 
 def main():
-    # inp_lines = open("inputs/input20").read().split('\n\n')
-    inp_lines = open("inputs/tests/testinput20").read().split('\n\n')
+    inp_lines = open("inputs/input20").read().split('\n\n')
+    # inp_lines = open("inputs/tests/testinput20").read().split('\n\n')
     algo, inp = inp_lines[0], [[1 if c == '#' else 0 for c in l] for l in inp_lines[1].split('\n')]
 
     coords = map_to_coords(inp)
     enhanced_coords = enhance_n_times(coords, algo, 2)
     draw_map(enhanced_coords)
-    print("Part 1:", len(enhanced_coords))
-
+    print("Part 1:", len(set(enhanced_coords)))
 
 
 if __name__ == "__main__":
