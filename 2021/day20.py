ef5e1275d8efef07633018643a561eb66761d989
Octave Larose <octave.larose@hotmail.fr>
2021-12-25 19:10:40 +0100

    Parsing/inputs for day 20

---
+++ 2021/day20.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+def part1(inp, algo):
+    pass
+
+
+def main():
+    # inp_lines = open("inputs/input20").read().split('\n\n')
+    inp_lines = open("inputs/tests/testinput20").read().split('\n\n')
+    algo, inp = inp_lines[0], inp_lines[1].split('\n')
+
+    print(algo)
+    print(inp)
+
+
+if __name__ == "__main__":
+    main()
