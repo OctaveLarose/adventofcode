04d7f2f0c0a0e5ebae551644b26e0e693f789e14
Octave Larose <octave.larose@hotmail.fr>
2022-12-02 12:48:38 +0000

    day 2 part 1 improved

--- 2022/src/day2.rs
+++ 2022/src/day2.rs
@@ -1,6 +1,12 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader, Lines};
 
+enum MatchResult {
+    WIN,
+    LOSS,
+    DRAW
+}
+
 fn get_pairs(lines: Lines<BufReader<File>>) -> Vec<(char, char)> {
     let mut pairs: Vec<(char, char)> = Vec::new();
 
@@ -10,50 +16,39 @@ fn get_pairs(lines: Lines<BufReader<File>>) -> Vec<(char, char)> {
         // Horrendous parsing.
         pairs.push((split.get(0).unwrap().chars().next().unwrap(), split.get(1).unwrap().chars().next().unwrap()));
     }
+
     pairs
 }
 
-fn get_match_score_part_1(pair: &(char, char)) -> usize {
-    let (opponent, ours) = pair;
-    let t_opponent = *opponent as u8;
-
-    let t_ours = match ours {
-        'Y' => b'B',
-        'X' => b'A',
-        'Z' => b'C',
-        _ => { panic!("should be unreachable")}
-    };
-
-    let bonus = (*ours as u8 % 4 + 1) as usize;
-
-    if t_opponent as u8 == t_ours {
-        return 3 + bonus;
-    }
-    else if t_opponent == t_ours + 1 || (t_ours == b'C' && t_opponent == b'A') {
-        return 0 + bonus
-    }
-    else {
-        return 6 + bonus
+fn get_round_result(opponent: u8, ours: u8) -> MatchResult {
+    if opponent as u8 == ours {
+        MatchResult::DRAW
+    } else if opponent == ours + 1 || (ours == 2 && opponent == 0) {
+        MatchResult::LOSS
+    } else {
+        MatchResult::WIN
     }
 }
 
-fn part1(pairs: &Vec<(char, char)>) -> usize {
-    let mut score = 0;
+fn strat_1(pair: &(char, char)) -> usize {
+    let (char_opponent, char_ours) = pair;
+    let opponent = *char_opponent as u8 - ('A' as u8);
+    let ours = *char_ours as u8 % 4;
+    let bonus = (*char_ours as u8 % 4 + 1) as usize;
 
-    for pair in pairs {
-        let t_score = get_match_score_part_1(pair);
-        score += t_score;
+    match get_round_result(opponent, ours) {
+        MatchResult::WIN=> 6 + bonus,
+        MatchResult::LOSS => 0 + bonus,
+        MatchResult::DRAW => 3 + bonus
     }
-    score
 }
 
 pub fn run() {
     let file = File::open("inputs/day2").unwrap();
-    let lines = BufReader::new(file).lines();
-    let pairs  = get_pairs(lines);
+    let pairs  = get_pairs(BufReader::new(file).lines());
 
     println!("Day 2: ");
-    println!("Part 1: {}", part1(&pairs));
-    // println!("Part 2: {}", part2(elves));
+    println!("Part 1: {}", pairs.iter().map(strat_1).sum::<usize>());
+    // println!("Part 2: {}", pairs.iter().map(strat_2).sum::<usize>());
     println!("----------")
 }
