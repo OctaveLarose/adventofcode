5c4b3bea86c1a71fbf770e586f7adf94b4469555
Octave Larose <octave.larose@hotmail.fr>
2022-12-09 14:21:46 +0000

    day 9 part 1

--- 2022/src/day9.rs
+++ 2022/src/day9.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use crate::day9::Direction::{U, R, D, L};
@@ -30,18 +31,84 @@ impl Motion {
     }
 }
 
+#[derive(Debug, Copy, Clone, Eq, Hash, PartialEq)]
+struct Pos {
+    x: isize,
+    y: isize
+}
+
+#[derive(Debug)]
+struct Rope {
+    head: Pos,
+    tail: Pos,
+    length: usize // number of non head/tail elements
+}
+
+impl Rope {
+    pub fn new(length: usize) -> Rope {
+        let start_pos = Pos {x: 0, y: 0}; // arbitrary
+        Rope { head: start_pos.clone(), tail: start_pos.clone(), length }
+    }
+
+    pub fn move_rope(&mut self, mot: &Motion) {
+        match mot.dir {
+            R => {
+                self.head.x += 1;
+                if self.tail.x < self.head.x - 1 {
+                    self.tail.x = self.head.x - 1;
+                    self.tail.y = self.head.y;
+                }
+            },
+            L => {
+                self.head.x -= 1;
+                if self.tail.x > self.head.x + 1 {
+                    self.tail.x = self.head.x + 1;
+                    self.tail.y = self.head.y;
+                }
+            },
+            D => {
+                self.head.y -= 1;
+                if self.tail.y > self.head.y + 1 {
+                    self.tail.x = self.head.x;
+                    self.tail.y = self.head.y + 1;
+                }
+            },
+            U => {
+                self.head.y += 1;
+                if self.tail.y < self.head.y - 1 {
+                    self.tail.x = self.head.x;
+                    self.tail.y = self.head.y - 1;
+                }
+            },
+        }
+    }
+}
+
+fn part1(mut rope: Rope, motions: &Vec<Motion>) -> usize {
+    let mut locations_tail_visited: HashSet<Pos> = HashSet::new();
+
+    for mot in motions {
+        // dbg!(&mot);
+        for _ in 0..mot.steps_nbr {
+            rope.move_rope(mot);
+            // println!("head: {} {}", rope.head.x, rope.head.y);
+            // println!("tail: {} {}", rope.tail.x, rope.tail.y);
+            // println!("---");
+            locations_tail_visited.insert(rope.tail.clone());
+        }
+    }
+
+    locations_tail_visited.len()
+}
+
 pub fn run() {
-    let file = File::open("inputs/testday9").unwrap();
-    let lines = BufReader::new(file).lines()
+    let file = File::open("inputs/day9").unwrap();
+    let motions = BufReader::new(file).lines()
         .map(|res_str| Motion::from_str(res_str.unwrap()))
         .collect::<Vec<Motion>>();
 
-    for l in lines{
-        dbg!(l);
-    }
-
     println!("Day 9: ");
-    // println!("Part 1: {}", tree_map.get_number_of_visible_trees());
+    println!("Part 1: {}", part1(Rope::new(0), &motions));
     // println!("Part 2: {}", tree_map.get_highest_scenic_score());
     println!("----------");
 }
