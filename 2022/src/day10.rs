eccbfb2bd04274620fd17c85ebaacc550f8ea205
Octave Larose <octave.larose@hotmail.fr>
2022-12-10 10:35:02 +0000

    day 10 part 1, bit meh

--- 2022/src/day10.rs
+++ 2022/src/day10.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::process::exit;
 use crate::day10::InstrType::{ADDX, NOOP};
 
 #[derive(Debug, Copy, Clone)]
@@ -11,7 +12,7 @@ enum InstrType {
 #[derive(Debug, Copy, Clone)]
 struct Instr {
     instr_type: InstrType,
-    value: i32, // Option would be cleaner (NOOP has no need for a value) but would use an object for nothing. Not sure the Rust compiler would optimize it away
+    value: isize, // Option would be cleaner (NOOP has no need for a value) but would use an object for nothing. Not sure the Rust compiler would optimize it away
     cycles_left: u32
 }
 
@@ -21,7 +22,7 @@ impl Instr {
             "noop" => Instr {instr_type: NOOP, value: 0, cycles_left: 1},
             "addx" => Instr {
                 instr_type: ADDX,
-                value: str.split_whitespace().nth(1).unwrap().parse::<i32>().unwrap(),
+                value: str.split_whitespace().nth(1).unwrap().parse::<isize>().unwrap(),
                 cycles_left: 2
             },
             _ => panic!("Invalid instruction string")
@@ -32,7 +33,6 @@ impl Instr {
 struct SignalHandler {
     register: isize,
     all_instrs: Vec<Instr>,
-    pending_instrs: Vec<Instr>,
     current_cycle: usize,
     interesting_signal_strength_total: isize
 }
@@ -42,7 +42,6 @@ impl SignalHandler {
         SignalHandler {
             register: 1,
             all_instrs: instructions,
-            pending_instrs: vec![],
             current_cycle: 0,
             interesting_signal_strength_total: 0
         }
@@ -54,47 +53,36 @@ impl SignalHandler {
 
     pub fn execute(&mut self) -> isize {
         for current_instr in &self.all_instrs {
-            if self.is_interesting_signal() {
-                dbg!(self.current_cycle, self.register);
-                self.interesting_signal_strength_total += (self.current_cycle as isize) * self.register;
-            }
-
             match current_instr.instr_type {
-                NOOP => {},
-                ADDX => { self.pending_instrs.push(current_instr.clone()) }
-            }
-
-            for mut instr in self.pending_instrs.iter_mut() {
-                instr.cycles_left -= 1;
-                if instr.cycles_left == 0 {
-                    self.register += instr.value as isize;
+                NOOP => {
+                    self.current_cycle += 1;
+                    if self.is_interesting_signal() {
+                        dbg!(self.register, self.current_cycle);
+                        self.interesting_signal_strength_total += (self.current_cycle as isize) * self.register;
+                    }
+                },
+                ADDX => {
+                    self.current_cycle += 1;
+                    if self.is_interesting_signal() {
+                        dbg!(self.register, self.current_cycle);
+                        self.interesting_signal_strength_total += (self.current_cycle as isize) * self.register;
+                    }
+                    self.current_cycle += 1;
+                    if self.is_interesting_signal() {
+                        dbg!(self.register, self.current_cycle);
+                        self.interesting_signal_strength_total += (self.current_cycle as isize) * self.register;
+                    }
+                    self.register += current_instr.value;
                 }
             }
-            self.pending_instrs.retain(|&instr| instr.cycles_left > 0);
-            self.current_cycle += 1;
         }
 
-        while !self.pending_instrs.is_empty() {
-            if self.is_interesting_signal() {
-                dbg!(self.current_cycle, self.register);
-                self.interesting_signal_strength_total += (self.current_cycle as isize) * self.register;
-            }
-
-            for mut instr in self.pending_instrs.iter_mut() {
-                instr.cycles_left -= 1;
-                if instr.cycles_left == 0 {
-                    self.register += instr.value as isize;
-                }
-            }
-            self.pending_instrs.retain(|&instr| instr.cycles_left > 0);
-            self.current_cycle += 1;
-        }
         self.interesting_signal_strength_total
     }
 }
 
 pub fn run() {
-    let file = File::open("inputs/testday10_2").unwrap();
+    let file = File::open("inputs/day10").unwrap();
     let instructions = BufReader::new(file).lines()
         .map(|res_str| Instr::from_str(res_str.unwrap()))
         .collect::<Vec<Instr>>();
