a286db39bf3f2846c8ba1e8ed2c4f07ec8240194
Octave Larose <octave.larose@hotmail.fr>
2022-12-10 10:01:12 +0000

    day 10 part 1 unfinished

--- 2022/src/day10.rs
+++ 2022/src/day10.rs
@@ -2,37 +2,104 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use crate::day10::InstrType::{ADDX, NOOP};
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 enum InstrType {
     ADDX,
     NOOP
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 struct Instr {
     instr_type: InstrType,
-    value: Option<u32>
+    value: i32, // Option would be cleaner (NOOP has no need for a value) but would use an object for nothing. Not sure the Rust compiler would optimize it away
+    cycles_left: u32
 }
 
 impl Instr {
     pub fn from_str(str: String) -> Instr {
         match &str[0..4] {
-            "noop" => Instr {instr_type: NOOP, value: None},
-            "addx" => Instr {instr_type: ADDX, value: str.split_whitespace().nth(1).unwrap().parse::<u32>().ok()},
+            "noop" => Instr {instr_type: NOOP, value: 0, cycles_left: 1},
+            "addx" => Instr {
+                instr_type: ADDX,
+                value: str.split_whitespace().nth(1).unwrap().parse::<i32>().unwrap(),
+                cycles_left: 2
+            },
             _ => panic!("Invalid instruction string")
         }
     }
 }
 
+struct SignalHandler {
+    register: isize,
+    all_instrs: Vec<Instr>,
+    pending_instrs: Vec<Instr>,
+    current_cycle: usize,
+    interesting_signal_strength_total: isize
+}
+
+impl SignalHandler {
+    pub fn new(instructions: Vec<Instr>) -> SignalHandler {
+        SignalHandler {
+            register: 1,
+            all_instrs: instructions,
+            pending_instrs: vec![],
+            current_cycle: 0,
+            interesting_signal_strength_total: 0
+        }
+    }
+
+    fn is_interesting_signal(&self) -> bool {
+        (self.current_cycle + 20) % 40 == 0
+    }
+
+    pub fn execute(&mut self) -> isize {
+        for current_instr in &self.all_instrs {
+            if self.is_interesting_signal() {
+                dbg!(self.current_cycle, self.register);
+                self.interesting_signal_strength_total += (self.current_cycle as isize) * self.register;
+            }
+
+            match current_instr.instr_type {
+                NOOP => {},
+                ADDX => { self.pending_instrs.push(current_instr.clone()) }
+            }
+
+            for mut instr in self.pending_instrs.iter_mut() {
+                instr.cycles_left -= 1;
+                if instr.cycles_left == 0 {
+                    self.register += instr.value as isize;
+                }
+            }
+            self.pending_instrs.retain(|&instr| instr.cycles_left > 0);
+            self.current_cycle += 1;
+        }
+
+        while !self.pending_instrs.is_empty() {
+            if self.is_interesting_signal() {
+                dbg!(self.current_cycle, self.register);
+                self.interesting_signal_strength_total += (self.current_cycle as isize) * self.register;
+            }
+
+            for mut instr in self.pending_instrs.iter_mut() {
+                instr.cycles_left -= 1;
+                if instr.cycles_left == 0 {
+                    self.register += instr.value as isize;
+                }
+            }
+            self.pending_instrs.retain(|&instr| instr.cycles_left > 0);
+            self.current_cycle += 1;
+        }
+        self.interesting_signal_strength_total
+    }
+}
+
 pub fn run() {
-    let file = File::open("inputs/testday10").unwrap();
+    let file = File::open("inputs/testday10_2").unwrap();
     let instructions = BufReader::new(file).lines()
         .map(|res_str| Instr::from_str(res_str.unwrap()))
         .collect::<Vec<Instr>>();
 
-    dbg!(&instructions);
     println!("Day 9: ");
-    // println!("Part 1: {}", get_nbr_locations_tail_visited(Rope::new(2), &instructions));
-    // println!("Part 2: {}", get_nbr_locations_tail_visited(Rope::new(10), &instructions));
+    println!("Part 1: {}", SignalHandler::new(instructions).execute());
     println!("----------");
 }
