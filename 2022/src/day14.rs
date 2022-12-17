b8bc176a80784d1b3585de556af67dfb1e0fbb97
Octave Larose <octave.larose@hotmail.fr>
2022-12-17 18:50:41 +0000

    day 14 part 2

--- 2022/src/day14.rs
+++ 2022/src/day14.rs
@@ -1,19 +1,15 @@
-use std::cmp::{max, min};
 use std::fmt::{Display, Formatter};
-use std::fs;
-use itertools::{Itertools};
+use std::{cmp, fs};
+use itertools::{Itertools, max};
 use crate::day14::SandStatus::{AtRest, InAbyss};
 use crate::day14::Tile::{Air, Rock, Sand};
 
 const SAND_SPAWN_X: usize = 500;
 const SAND_SPAWN_Y: usize = 0;
 
-const MIN_X: usize = 450;
-const MAX_X: usize = 520;
-const MAX_Y: usize = 170;
-// const MIN_X: usize = 492;
-// const MAX_X: usize = 505;
-// const MAX_Y: usize = 10;
+const MIN_X: usize = 300;
+const MAX_X: usize = 700;
+const MAX_Y: usize = 180;
 const LENGTH: usize = MAX_X - MIN_X;
 
 type MapTiles = [[Tile; LENGTH]; MAX_Y];
@@ -42,12 +38,26 @@ impl Display for Tile {
 }
 
 struct Map {
-    map_tiles: MapTiles
+    map_tiles: MapTiles,
+    ground_idx: usize
+}
+
+impl Display for Map {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        for l in self.map_tiles {
+            for c in l {
+                write!(f, "{}", c).expect("COULDN'T WRITE");
+            }
+            write!(f, "\n").expect("COULDN'T WRITE");
+        }
+        write!(f, "\n")
+    }
 }
 
 impl Map {
-    fn create_map_from_input(lines: Vec<&str>) -> Map {
+    fn create_map_from_input(lines: &Vec<&str>) -> Map {
         let mut map_tiles: MapTiles = [[Air; LENGTH]; MAX_Y];
+        let mut highest_depth = 0;
 
         for l in lines {
             for (start, end) in l.split(" -> ").tuple_windows::<(&str, &str)>() {
@@ -59,14 +69,14 @@ impl Map {
                     .collect_tuple::<(usize, usize)>().unwrap();
 
                 if start_x == end_x {
-                    let min = min(start_y, end_y);
-                    let max = max(start_y, end_y) + 1;
+                    let min = cmp::min(start_y, end_y);
+                    let max = cmp::max(start_y, end_y) + 1;
                     for i in min..max {
                         map_tiles[i][start_x - MIN_X] = Rock;
                     }
                 } else if start_y == end_y {
-                    let min = min(start_x, end_x) - MIN_X;
-                    let max = max(start_x, end_x) - MIN_X + 1;
+                    let min = cmp::min(start_x, end_x) - MIN_X;
+                    let max = cmp::max(start_x, end_x) - MIN_X + 1;
 
                     for i in min..max {
                         map_tiles[start_y][i] = Rock;
@@ -74,9 +84,13 @@ impl Map {
                 } else {
                     panic!("Invalid coordinates, not a straight line");
                 }
+
+                highest_depth = max([highest_depth, start_y, end_y]).unwrap();
             }
         }
-        Map { map_tiles }
+        Map { map_tiles,
+            ground_idx: highest_depth + 2
+        }
     }
 
     fn can_move_sand(&mut self, pos: (usize, usize)) -> bool {
@@ -111,43 +125,69 @@ impl Map {
         }
     }
 
+    fn make_sand_fall_with_ground(&mut self) -> SandStatus {
+        let mut sand_pos = (SAND_SPAWN_X, SAND_SPAWN_Y);
+
+        loop {
+            let pos_down = (sand_pos.0, sand_pos.1 + 1);
+            let pos_down_l = (sand_pos.0 - 1, sand_pos.1 + 1);
+            let pos_down_r = (sand_pos.0 + 1, sand_pos.1 + 1);
+
+            if sand_pos.1 == self.ground_idx - 1 {
+                self.map_tiles[sand_pos.1][sand_pos.0 - MIN_X] = Sand;
+                return AtRest;
+            }
+
+            if self.can_move_sand(pos_down) {
+                sand_pos = pos_down;
+                continue;
+            } else if self.can_move_sand(pos_down_l) {
+                sand_pos = pos_down_l;
+                continue;
+            } else if self.can_move_sand(pos_down_r) {
+                sand_pos = pos_down_r;
+                continue;
+            } else {
+                self.map_tiles[sand_pos.1][sand_pos.0 - MIN_X] = Sand;
+                return AtRest
+            }
+        }
+    }
+
     fn get_nbr_sand_resting(&mut self) -> usize {
         let mut nbr_sand = 0;
 
         loop {
             match self.make_sand_fall() {
-                AtRest => {}
                 InAbyss => {break;}
-                SandStatus::Moved => {panic!("Sand is supposed to be done falling")}
+                _ => { nbr_sand += 1; }
             }
-            nbr_sand += 1;
         }
-        // print_map(&map);
+
+        nbr_sand
+    }
+
+    fn get_nbr_sand_resting_with_ground(&mut self) -> usize {
+        let mut nbr_sand = 0;
+
+        loop {
+            self.make_sand_fall_with_ground();
+            nbr_sand += 1;
+            if self.map_tiles[SAND_SPAWN_Y][SAND_SPAWN_X - MIN_X] == Sand {
+                break;
+            }
+        }
 
         nbr_sand
     }
 }
 
-impl Display for Map {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        for l in self.map_tiles {
-            for c in l {
-                write!(f, "{}", c).expect("COULDN'T WRITE");
-            }
-            write!(f, "\n").expect("COULDN'T WRITE");
-        }
-        write!(f, "\n")
-    }
-}
-
 pub fn run() {
-    let file_str = fs::read_to_string("inputs/day14").unwrap();
+    let file_str = fs::read_to_string("inputs/testday14").unwrap();
     let file_lines = file_str.lines().collect::<Vec<&str>>();
 
-    let mut map = Map::create_map_from_input(file_lines);
-
     println!("Day 14: ");
-    println!("Part 1: {}", map.get_nbr_sand_resting());
-    // println!("Part 2: {}", part2(&mut all_packets));
+    println!("Part 1: {}", Map::create_map_from_input(&file_lines).get_nbr_sand_resting());
+    println!("Part 2: {}", Map::create_map_from_input(&file_lines).get_nbr_sand_resting_with_ground());
     println!("----------");
 }
