d80ab4d5eed505a6be5ce06c49336cde1ced1ddd
Octave Larose <octave.larose@hotmail.fr>
2022-12-18 17:51:10 +0000

    day 16 part 1 - maybe functional? crazy slow

--- 2022/src/day16.rs
+++ 2022/src/day16.rs
@@ -44,16 +44,52 @@ fn parse_scan<'a>(input: &'a String) -> HashMap<&'a str, Rc<RefCell<Valve<'a>>>>
     valves
 }
 
+fn walk<'a>(cur_valve: Rc<RefCell<Valve<'a>>>, open_valves: &mut Vec<Rc<RefCell<Valve<'a>>>>, nbr_minutes: i32, mut best_output: usize) -> usize {
+    if nbr_minutes <= 0 {
+        return best_output;
+    }
+
+    for open_valve in open_valves.into_iter() {
+        best_output += open_valve.borrow().flow_rate;
+    }
+
+    let mut outputs = vec![best_output];
+    // either we move
+    for dest in &cur_valve.borrow().tunnels_to {
+        outputs.push(walk(dest.clone(), &mut open_valves.clone(), nbr_minutes - 1, best_output))
+    }
+
+    // or we open the valve (if it's openable)
+    if cur_valve.borrow().flow_rate != 0 {
+        if open_valves.iter().find(|&c| c.borrow().name == cur_valve.borrow().name).is_none() {
+            open_valves.push(cur_valve.clone());
+            outputs.push(walk(cur_valve.clone(), &mut open_valves.clone(), nbr_minutes - 1, best_output))
+        }
+    }
+
+    *outputs.iter().max().unwrap()
+}
+
+fn part1(valves: &HashMap<&str, Rc<RefCell<Valve>>>) -> usize {
+    let mut open_valves = vec![];
+
+    let best_output = walk(valves["AA"].clone(), &mut open_valves, 30, 0);
+    for v in open_valves {
+        println!("{}", v.borrow().to_string())
+    }
+    best_output
+}
+
 pub fn run() {
     let file_str = fs::read_to_string("inputs/testday16").unwrap();
     let valves = parse_scan(&file_str);
 
-    for v in &valves {
-        println!("{}", v.1.borrow().to_string());
-    }
+    // for v in &valves {
+    //     println!("{}", v.1.borrow().to_string());
+    // }
 
     println!("Day 16: ");
-    // println!("Part 1: {}", get_nbr_positions_with_no_beacon(&sensor_beacon_pairs, 2000000));
+    println!("Part 1: {}", part1(&valves));
     // println!("Part 2: {}", Map::create_map_from_input(&file_lines).get_nbr_sand_resting_with_ground());
     println!("----------");
 }
