386595dc09a51ba554de16624db1679171063aa8
Octave Larose <octave.larose@hotmail.fr>
2022-12-18 09:50:50 +0000

    day 15 part 1 start

--- 2022/src/day15.rs
+++ 2022/src/day15.rs
@@ -1,6 +1,6 @@
 use std::{fs};
-use std::ops::Index;
-use std::process::exit;
+use std::collections::HashSet;
+use std::ops::Range;
 use itertools::Itertools;
 
 #[derive(Debug)]
@@ -32,16 +32,44 @@ impl SensorBeacon {
 
 }
 
+fn get_nbr_positions_with_no_beacon(pairs: &Vec<SensorBeacon>, line_nbr: isize) -> usize {
+    let mut positions: HashSet<Range<isize>> = HashSet::new();
+
+    for pair in pairs {
+        if pair.beacon.y == line_nbr {
+            positions.insert(Range {start: pair.beacon.x, end: pair.beacon.x + 1});
+        }
+
+        let distance = (pair.sensor.x - pair.beacon.x).abs() + (pair.sensor.y - pair.beacon.y).abs();
+
+        if pair.sensor.y > line_nbr && pair.sensor.y - distance < line_nbr {
+            let lol = distance - (pair.sensor.y - pair.beacon.y).abs();
+            let r = Range {start: pair.sensor.x - lol, end: pair.sensor.x + lol + 1};
+            dbg!(&pair, &r, "---", r.len());
+            positions.insert(r);
+        }
+
+
+        if pair.sensor.y < line_nbr && pair.sensor.y + distance > line_nbr {
+            let lol = distance - (pair.sensor.y - pair.beacon.y).abs();
+            let r = Range {start: pair.sensor.x - lol, end: pair.sensor.x + lol + 1};
+
+            dbg!(&pair, &r, "---", r.len());
+            positions.insert(r);
+        }
+    }
+
+    positions.iter().map(|range| range.len()).sum::<usize>()
+}
+
 pub fn run() {
-    let file_str = fs::read_to_string("inputs/day15").unwrap();
+    let file_str = fs::read_to_string("inputs/testday15").unwrap();
     let sensor_beacon_pairs = file_str.lines()
         .map(|str| SensorBeacon::from_string(str))
         .collect::<Vec<SensorBeacon>>();
 
-    dbg!(&sensor_beacon_pairs);
-
     println!("Day 15: ");
-    // println!("Part 1: {}", Map::create_map_from_input(&file_lines).get_nbr_sand_resting());
+    println!("Part 1: {}", get_nbr_positions_with_no_beacon(&sensor_beacon_pairs, 10));
     // println!("Part 2: {}", Map::create_map_from_input(&file_lines).get_nbr_sand_resting_with_ground());
     println!("----------");
 }
