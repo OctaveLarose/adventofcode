16614436c8ea0c815da178fc7fa8eb7f90dcea2a
Octave Larose <octave.larose@hotmail.fr>
2023-12-08 10:47:10 +0000

    day2 part 1

--- 2023/day2/src/main.rs
+++ 2023/day2/src/main.rs
@@ -1,47 +1,73 @@
+use std::fmt;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-enum Color {
-    RED = 0,
-    GREEN = 1,
-    BLUE = 2
+struct RGB {
+    r: Option<usize>,
+    g: Option<usize>,
+    b: Option<usize>
 }
 
-#[derive(Debug)]
+impl fmt::Debug for RGB {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "R{:?} G{:?} B{:?}",
+               if self.r.is_some() { self.r.unwrap() } else { 0 },
+               if self.g.is_some() { self.g.unwrap() } else { 0 },
+               if self.b.is_some() { self.b.unwrap() } else { 0 })
+    }
+}
+
 struct Game {
     id: usize,
-    subsets: Vec<(Option<usize>, Option<usize>, Option<usize>)>
+    rounds: Vec<RGB>
+}
+
+impl fmt::Debug for Game {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}: {}\n",
+               &self.id,
+               self.rounds.iter()
+                   .map(|round| format!("{:?}", round))
+                   .collect::<Vec<_>>()
+                   .join("; ")
+        )
+    }
 }
 
 impl Game {
     pub fn parse(input_str: String) -> Game {
-        let mut rounds: Vec<(Option<usize>, Option<usize>, Option<usize>)> = vec![];
         let split_input_rounds = input_str.as_str()[input_str.find(": ").unwrap() + 2..]
             .split("; ")
             .collect::<Vec<&str>>();
 
-        for l in split_input_rounds {
-            let mut game_draws: (Option<usize>, Option<usize>, Option<usize>) = (None, None, None);
-            for value_pair in l.split(", ") {
-                let nbr = value_pair.split(" ").next().unwrap().parse::<usize>().unwrap();
-                let color = value_pair.split(" ").nth(1).unwrap();
-
-                match color {
-                    "red" => game_draws.0 = Some(nbr),
-                    "green" => game_draws.1 = Some(nbr),
-                    "blue" => game_draws.2 = Some(nbr),
-                    _ => panic!("Invalid color")
-                }
-
-                dbg!(nbr, color);
-            }
-
-            rounds.push(game_draws);
-        }
-
         Game {
             id: input_str.as_str()[5..input_str.find(':').unwrap()].parse::<usize>().unwrap(),
-            subsets: rounds
+            rounds: split_input_rounds.iter().map(|l| {
+                let mut round: RGB = RGB {r: None, g: None, b: None};
+                for value_pair in l.split(", ") {
+                    let nbr = value_pair.split(" ").next().unwrap().parse::<usize>().unwrap();
+                    let color = value_pair.split(" ").nth(1).unwrap();
+
+                    match color {
+                        "red" => round.r = Some(nbr),
+                        "green" => round.g = Some(nbr),
+                        "blue" => round.b = Some(nbr),
+                        _ => panic!("Invalid color")
+                    }
+                }
+
+                round
+            }
+            ).collect::<Vec<RGB>>()
+        }
+    }
+
+    pub fn return_id_if_possible(&self, expectation: &RGB) -> Option<usize> {
+        match self.rounds.iter()
+            .any(|round| expectation.r < round.r || expectation.g < round.g || expectation.b < round.b)
+        {
+            true => None,
+            false => Some(self.id)
         }
     }
 }
@@ -49,11 +75,16 @@ impl Game {
 
 
 fn main() {
-    let file = File::open("../inputs/testday2").unwrap();
+    let file = File::open("../inputs/day2").unwrap();
     let games: Vec<Game> = BufReader::new(file)
         .lines()
         .map(|l|Game::parse(l.unwrap()))
         .collect::<Vec<Game>>();
 
     dbg!(&games);
+
+    let part1_expectation: RGB = RGB {r: Some(12), g: Some(13), b: Some(14)};
+    println!("Part 1: {}", games.iter()
+        .filter_map(|g| g.return_id_if_possible(&part1_expectation))
+        .sum::<usize>());
 }
