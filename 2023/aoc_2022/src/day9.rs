8a7d891b91ac3b6b84a566e2dab842ab39f9f3f6
Octave Larose <octave.larose@hotmail.fr>
2023-12-11 16:29:32 +0000

    day9 part 2. well that was the easiest day so far

--- 2023/aoc_2022/src/day9.rs
+++ 2023/aoc_2022/src/day9.rs
@@ -7,8 +7,17 @@ fn part1_rec(sequence: &History) -> isize {
     let sub_seq = &sequence.windows(2).map(|a| a[1] - a[0]).collect::<History>();
 
     match sub_seq.iter().all_equal() {
-        true => sequence.last().unwrap() + *sub_seq.iter().next().unwrap(),
+        true => sequence.last().unwrap() + *sub_seq.first().unwrap(),
         false => sequence.last().unwrap() + part1_rec(sub_seq)
+    }
+}
+
+fn part2_rec(sequence: &History) -> isize {
+    let sub_seq = &sequence.windows(2).map(|a| a[1] - a[0]).collect::<History>();
+
+    match sub_seq.iter().all_equal() {
+        true => sequence.first().unwrap() - *sub_seq.first().unwrap(),
+        false => sequence.first().unwrap() - part2_rec(sub_seq)
     }
 }
 
@@ -18,7 +27,6 @@ pub fn run() {
         .map(|l| l.split_whitespace().map(|l2| l2.parse::<isize>().unwrap()).collect())
         .collect();
 
-    // dbg!(&histories);
     println!("Part 1: {}", &histories.iter().map(part1_rec).sum::<isize>());
-    // println!("Part 2: {}", part2(&nodes, &directions));
+    println!("Part 2: {}", &histories.iter().map(part2_rec).sum::<isize>());
 }
