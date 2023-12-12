678722125fa3ec1e7a515db8ac31fd6923fc8778
Octave Larose <octave.larose@hotmail.fr>
2023-12-12 16:09:57 +0000

    day 3: improved parsing and made a map printer

--- 2023/aoc_2023/src/day3.rs
+++ 2023/aoc_2023/src/day3.rs
@@ -1,3 +1,4 @@
+use std::fmt::{Debug, Display, Formatter, Write};
 use std::fs;
 use crate::map::{Map2D, CharMapElement};
 
@@ -7,7 +8,7 @@ type Symbol = char; // should that be an enum of the possible ones?
 struct Number {
     val: usize,
     pos: usize,
-    nbr_len: usize // number of digits. to not have to recalculate it constantly
+    nbr_len: usize, // number of digits. to not have to recalculate it constantly
 }
 
 
@@ -15,7 +16,36 @@ struct Number {
 struct Schematic {
     map: Map2D<CharMapElement>,
     numbers: Vec<Number>,
-    symbols: Vec<(Symbol, usize)>
+    symbols: Vec<(Symbol, usize)>,
+}
+
+// bad, inefficient code made as a sanity check.
+impl Display for Schematic {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let mut i = 0;
+
+        while i < (self.map.width * self.map.height) {
+            let matching_sym = self.symbols.iter().find(|(sym, sym_pos)| { *sym_pos == i });
+            let matching_num = self.numbers.iter().find(|n| { n.pos == i });
+
+            if matching_sym.is_some() {
+                write!(f, "{}", matching_sym.unwrap().0).expect("write failed");
+            } else if matching_num.is_some() {
+                write!(f, "{}", matching_num.unwrap().val).expect("write failed");
+                i += matching_num.unwrap().nbr_len - 1;
+            } else {
+                write!(f, ".").expect("write failed");
+            }
+
+            if (i + 1) % self.map.width == 0 {
+                write!(f, "\n").expect("write failed");
+            }
+
+            i += 1;
+        }
+
+        Ok(())
+    }
 }
 
 impl Schematic {
@@ -25,33 +55,31 @@ impl Schematic {
         let mut symbols: Vec<(Symbol, usize)> = vec![];
 
         let mut it = map.tiles.iter().filter(|c| c.c != '\n').enumerate();
-        while let Some(tile) = it.next() {
-            match tile.1.c {
-                '.' | '\n' => {},
+        while let Some((pos, char_elem)) = it.next() {
+            match char_elem.c {
+                '.' => {}
                 '0'..='9' => {
-                    let row = tile.0 / map.width;
-                    let end_of_line = (row + 1) * map.width;
-                    let nbr_len = if let Some(s) = map.tiles[tile.0..end_of_line].iter()
+                    let end_of_line = (pos / map.width + 1) * map.width;
+                    let nbr_len = map.tiles[pos..end_of_line].iter()
                         .position(|c| !c.c.is_numeric())
-                    {
-                        s
-                    } else {
-                        end_of_line - tile.0
-                    };
+                        .unwrap_or(end_of_line - pos);
 
-                    let num_str = map.tiles[tile.0..tile.0 + nbr_len].iter().map(|c| c.c).collect::<String>();
-                    let num_val = num_str.parse::<usize>().unwrap();
                     numbers.push(Number {
-                        val: num_val,
-                        pos: tile.0,
-                        nbr_len
+                        val: map.tiles[pos..pos + nbr_len].iter()
+                            .map(|c| c.c)
+                            .collect::<String>()
+                            .parse::<usize>().unwrap(),
+                        pos: pos,
+                        nbr_len,
                     });
+
+                    // skip the next digits if they exist, and account for the fact we do a next() at the start of each loop (therefore -2 and not -1)
                     if nbr_len != 1 {
                         it.nth(nbr_len - 2);
                     }
-                },
+                }
                 c => match c {
-                    '+' | '-' | '*' | '/' | '@' | '=' | '$' | '#' | '%' | '&' => symbols.push((c, tile.0)),
+                    '+' | '-' | '*' | '/' | '@' | '=' | '$' | '#' | '%' | '&' => symbols.push((c, pos)),
                     idk => panic!("{}", idk)
                 },
             }
@@ -60,17 +88,18 @@ impl Schematic {
         Schematic {
             map,
             numbers,
-            symbols }
+            symbols,
+        }
     }
 
     pub fn part1_get_part_numbers_sum(&self) -> usize {
-        self.numbers.iter().filter_map(| nbr|{
+        self.numbers.iter().filter_map(|nbr| {
             for (_, sym_pos) in &self.symbols {
                 for surround_pos in self.map.get_positions_around(*sym_pos) {
                     match surround_pos {
                         None => {}
                         Some(v) => match nbr.pos <= v && v <= nbr.pos + nbr.nbr_len {
-                            true => { dbg!(nbr.val); return Some(nbr.val)},
+                            true => { return Some(nbr.val); } //dbg!(nbr.val);
                             false => {}
                         }
                     }
@@ -84,6 +113,8 @@ impl Schematic {
 
 pub fn run() {
     let schematic = Schematic::parse(fs::read_to_string("../inputs/day3").unwrap());
+
+    println!("{}", &schematic);
 
     println!("Day 3: ");
     println!("Part 1: {}", schematic.part1_get_part_numbers_sum());
