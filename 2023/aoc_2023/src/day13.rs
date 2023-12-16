b278d025045f4ea36a523d30f4e63e672a4a9d75
Octave Larose <octave.larose@hotmail.fr>
2023-12-16 22:40:03 +0000

    day 13 part 1

--- 2023/aoc_2023/src/day13.rs
+++ 2023/aoc_2023/src/day13.rs
@@ -3,9 +3,15 @@ use std::fs;
 use crate::day13::Tile::*;
 use crate::map::{Map2D, MapElement};
 
+#[derive(PartialEq)]
 enum Tile {
     ASH,
-    ROCK
+    ROCK,
+}
+
+enum MirrorDirection {
+    HORIZONTAL,
+    VERTICAL
 }
 
 impl Debug for Tile {
@@ -34,14 +40,58 @@ impl MapElement for Tile {
     }
 }
 
-pub fn run() {
-    let file_str = fs::read_to_string("../inputs/testday13").unwrap();
-    let all_maps: Vec<Map2D<Tile>> = file_str.split("\n\n").map(Map2D::parse).collect();
-    // let all_pairs: Vec<SpringsAndCondition> = file_str.lines()
-    //     .map(SpringsAndCondition::from_str)
-    //     .collect();
+impl Map2D<Tile> {
+    fn are_columns_identical(&self, idx1: usize, idx2: usize) -> bool {
+        (0..self.height).all(|v| *self.get(idx1 + v * self.width) == *self.get(idx2 + v * self.width))
+    }
 
-    dbg!(&all_maps);
-    // println!("Part 1: {}", all_pairs.iter().map(SpringsAndCondition::get_nbr_arrangements).sum::<usize>());
+    fn are_rows_identical(&self, idx1: usize, idx2: usize) -> bool {
+        (0..self.width).all(|v| *self.get(idx1 * self.width + v) == *self.get(idx2 * self.width + v))
+    }
+
+    fn find_reflection(&self, dir: MirrorDirection) -> Option<usize> {
+        let (max, are_col_or_rows_equiv) = match dir {
+            MirrorDirection::HORIZONTAL => (self.height, Map2D::are_rows_identical as fn(&Map2D<Tile>, usize, usize) -> bool),
+            MirrorDirection::VERTICAL => (self.width, Map2D::are_columns_identical as fn(&Map2D<Tile>, usize, usize) -> bool)
+        };
+
+        for (chunk_idx, _) in self.tiles[0..max].windows(2).enumerate() {
+            if !are_col_or_rows_equiv(self, chunk_idx, chunk_idx + 1) {
+                continue;
+            }
+
+            let left_col_iter = (0..chunk_idx).rev();
+            let right_col_iter = (chunk_idx + 2..).take_while(|&idx| idx < max);
+
+            if left_col_iter
+                .zip(right_col_iter)
+                .all(|(left_idx, right_idx)| are_col_or_rows_equiv(self, left_idx, right_idx))
+            {
+                return Some(chunk_idx);
+            }
+        }
+        None
+    }
+
+    fn get_part1_val(&self) -> usize {
+        if let Some(horizontal_idx) = self.find_reflection(MirrorDirection::HORIZONTAL) {
+            (horizontal_idx + 1) * 100
+        } else if let Some(vertical_idx) = self.find_reflection(MirrorDirection::VERTICAL) {
+            vertical_idx + 1
+        } else {
+            panic!("No reflection found??");
+        }
+    }
+}
+
+pub fn run() {
+    let file_str = fs::read_to_string("../inputs/day13").unwrap();
+    let all_maps: Vec<Map2D<Tile>> = file_str.split("\n\n").map(Map2D::parse).collect();
+
+    // dbg!(&all_maps);
+    // dbg!(all_maps.first().unwrap().find_vertical_reflection());
+    // dbg!(all_maps.last().unwrap().find_horizontal_reflection());
+
+    println!("Part 1: {}", all_maps.iter().map(Map2D::get_part1_val).sum::<usize>());
     // println!("Part 2: {}", all_pairs.iter().map(SpringsAndCondition::unfold).map(|v| v.get_nbr_arrangements()).sum::<usize>());
 }
